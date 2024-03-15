7c0b3f3a6899cb7cb150892f7ce79606dae3672d
webstareg <amr@webstar.com.eg>
2024-03-15 23:30:14 +0200

    Taskati

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:taskati/core/utils/colors.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      // LIGHT
+      theme: ThemeData(
+          fontFamily: GoogleFonts.poppins().fontFamily,
+          scaffoldBackgroundColor: AppColors.white,
+          appBarTheme: AppBarTheme(
+            backgroundColor: AppColors.white,
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: AppColors.primary)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: AppColors.primary)),
+              errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: AppColors.red)),
+              focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: AppColors.red)))),
+      // DARK
+      darkTheme: ThemeData(
+          scaffoldBackgroundColor: AppColors.darkBackground,
+          appBarTheme: AppBarTheme(
+            backgroundColor: AppColors.primary,
+            color: AppColors.white,
+          )),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
